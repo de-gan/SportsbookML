07d7ce133db41d48024d570ec725f4be570984cc
Dennis Gannon <degan@Denniss-MacBook-Pro.local>
2025-07-06 23:04:40 -0400

    Feature selection

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # SportsbookML
+Model to predict MLB Baseball odds (Specific types TBD)
+# Data Collection
+- Home vs Away
+- Final scores
+- Starting pitcher stats
+- Bullpen ERA
+- Team batting average vs LHP/RHP
+- Previous 5-game trends
+- Day of week, stadium, travel fatigue
+## Betting Odds
+- Moneyline odds → convert to implied probability
+- Run line (typically ±1.5)
+- Over/under total runs
+
+# Win/Loss Predictor
+Want data for Both teams individually, and then head-to-head
+- Team stats
+- Pitcher W/L
+- Lineup BA
+- Travel fatigue
+
+# Necessary Variables
+## Target: 
+Win/Loss
+## Features: Split when Home v Away 
+- Win/Loss streak -- Captures momentum
+- Last N games W% -- Reflects recent form
+- Season W% -- Overall team quality
+- Home/Away indicator -- Home field advantage
+- Run differential (season & recent) -- Better than just W/L
+- Avg runs scored/allowed (last N games) -- Tracks offense/defense form
+- Opponent matchup record -- Psychological edge or disadvantage
+- Division matchup flag	-- Divisional games tend to be tighter?
+- Rest days -- More rest = better performance
+## schedule_and_record
+- 
--- sportsbook_model.py
+++ sportsbook_model.py
@@ -1,6 +1,40 @@
-import sklearn
+import warnings
+from urllib3.exceptions import NotOpenSSLWarning
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=NotOpenSSLWarning)
+
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from pybaseball import schedule_and_record, team_batting, team_pitching
+
+batting_2023 = team_batting(2024)
+pitching_2023 = team_pitching(2024)
+batting_nyy = batting_2023[batting_2023['Team'] == 'NYY']
+pitching_nyy = pitching_2023[pitching_2023['Team'] == 'NYY']
+
+# Team Schedule and Record
+def get_team_schedule_and_record(team):
+    schedule = schedule_and_record(2023, team)
+    schedule.drop(columns=['Time', 'Attendance', 'Inn', 'Orig. Scheduled', 'Save', 'GB'], inplace=True)
+    le = LabelEncoder()
+    schedule['Home_Away'] = le.fit_transform(schedule['Home_Away'])
+    schedule['W/L'] = le.fit_transform(schedule['W/L'])
+    schedule['D/N'] = le.fit_transform(schedule['D/N'])
+    schedule['WinDiff'] = schedule['R'] - schedule['RA']
+    return schedule
+
+schedule_nyy = get_team_schedule_and_record('NYY')
+#print(batting_2023)
+print(schedule_nyy.head())
+
+#df = schedule_nyy.drop(columns=['Date', 'Tm', 'W-L', 'GB', 'Win', 'Loss'])
+#print(df.head())
+#correlation_matrix = df.corr()
+#sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+#plt.show()
 
